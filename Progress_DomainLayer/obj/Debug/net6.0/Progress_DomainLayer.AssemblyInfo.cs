a16c2f8f79ae319a1fc0c5ffd1553a5fa5d034f5
edd-boka <eddboka@gmail.com>
2026-09-02 11:08:52 +0200

    KB-After migration
    I have created a datatable with info like subjects, tests and average

--- Progress_DomainLayer/obj/Debug/net6.0/Progress_DomainLayer.AssemblyInfo.cs
+++ Progress_DomainLayer/obj/Debug/net6.0/Progress_DomainLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Progress_DomainLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6399ff4b70da36a8ae5f15559deedf6f816776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c754d92f0498d47f5d24981008f736f92d0e51c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Progress_DomainLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Progress_DomainLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
