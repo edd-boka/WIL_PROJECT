c754d92f0498d47f5d24981008f736f92d0e51c7
edd-boka <eddboka@gmail.com>
2026-09-02 10:55:54 +0200

    KB-Web project before migration

---
+++ Progress_DomainLayer/Data/SQLiteDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Progress_DomainLayer.Models;
+
+namespace Progress_DomainLayer.Data
+{
+    public class SQLiteDBContext: DbContext
+    {
+        public SQLiteDBContext(DbContextOptions<SQLiteDBContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Mark> Marks { get; set; }
+    }
+}
---
+++ Progress_DomainLayer/Models/Marks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progress_DomainLayer.Models
+{
+    public class Mark: BaseEntity
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string Test1 { get; set; }
+        public string Test2 { get; set; }
+        public string Test3 { get; set; }
+        public string Test4 { get; set; }
+        public string Test5 { get; set; }
+        public string Test6 { get; set; }
+        public string Test7 { get; set; }
+        public string Test8 { get; set; }
+        public string Average { get; set; }
+        public bool Checked { get; set; }
+    }
+}
